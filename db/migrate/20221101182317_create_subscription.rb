d0bc5135325586a29c65954de55f455c415be485
Tyler Caudill <103776658+TeeCaud@users.noreply.github.com>
2022-11-01 13:25:16 -0500

    migrations

---
+++ db/migrate/20221101182317_create_subscription.rb
@@ -0,0 +1,10 @@
+class CreateSubscription < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.string :price
+      t.string :status
+      t.string :frequency
+    end
+  end
+end
