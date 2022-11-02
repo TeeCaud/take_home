fd45acbbe9a9035fa620276d6f853fbf2ba60614
Tyler Caudill <103776658+TeeCaud@users.noreply.github.com>
2022-11-02 15:34:51 -0500

    limit /customers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	namespace :api do
 		namespace :v1 do
-			resources :customers do
+			resources :customers, only: [] do
 				resources :customer_subscriptions, only: [:create, :destroy, :index]
 			end
 		end
